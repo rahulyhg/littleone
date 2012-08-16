0ddca453b1f00ec3304ec3e833c48d46e9db49c9
Virgil Dimaguila <virgil@thehumblepie.com>
2012-08-15 20:15:10 -0400

    Post generate.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Littleone::Application.config.secret_token = '64f28ab3b62d87404afabb96e06f908fa4b25ef202abe7d3b58c7eb08ce1e2f87b54b7bb7130e87108a48f913df9b6e554da9f647cc2c82cb1de6d0ab9472ed3'
